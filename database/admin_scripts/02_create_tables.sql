0a7b154f5315fd503dc68aa214183590f72e71dd
charles <charles.bernard@cri-paris.org>
2019-01-30 16:25:11 +0100

    update

--- database/admin_scripts/02_create_tables.sql
+++ database/admin_scripts/02_create_tables.sql
@@ -1,43 +1,26 @@
 /* LEVEL 0 TABLES */
 CREATE TABLE bio_process (
-	bio_process_id INT PRIMARY KEY,
+	bio_process_id SERIAL PRIMARY KEY,
 	bio_process VARCHAR
 );
 
 CREATE TABLE domain (
-	domain_id INT PRIMARY KEY,
+	domain_id SERIAL PRIMARY KEY,
 	db_domain_id VARCHAR,
 	domain_name VARCHAR
 );
 
-CREATE TABLE environment (
-	env_id INT PRIMARY KEY,
-	db_env_id VARCHAR,
-	location VARCHAR,
-	sampling_project VARCHAR,
-	properties VARCHAR
-);
-
 CREATE TABLE pathway (
-	pathway_id INT PRIMARY KEY,
+	pathway_id SERIAL PRIMARY KEY,
 	db_pathway_id VARCHAR,
 	pathway_name VARCHAR
 );
 
 CREATE TABLE reference (
-	reference_id INT PRIMARY KEY,
+	reference_id SERIAL PRIMARY KEY,
 	pubmed_id INT,
+	doi VARCHAR,
 	isbn VARCHAR
-);
-
-CREATE TABLE signal (
-	signal_id INT PRIMARY KEY,
-	signal_supercategory VARCHAR,
-	signal_family VARCHAR,
-	signal_trivial_name VARCHAR,
-	signal_systematic_name VARCHAR,
-	signal_chemical_formula VARCHAR,
-	structure_img BYTEA
 );
 
 CREATE TABLE taxonomy (
@@ -49,11 +32,20 @@ CREATE TABLE taxonomy (
 );
 
 /* LEVEL 1 TABLES */
-CREATE TABLE environment_members (
-	env_id INT REFERENCES environment ON DELETE CASCADE ON UPDATE CASCADE,
-	tax_id CHAR(30) REFERENCES taxonomy ON UPDATE CASCADE,
-	PRIMARY KEY (env_id, tax_id)
+CREATE TABLE bio_process_pathways (
+	bio_process_id INT REFERENCES reference ON DELETE CASCADE ON UPDATE CASCADE,
+	pathway_id INT REFERENCES pathway ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (bio_process_id, pathway_id)
 );
+
+CREATE TABLE environment (
+	env_id SERIAL PRIMARY KEY,
+	location VARCHAR,
+	sampling_project VARCHAR,
+	properties VARCHAR,
+	reference_id INT REFERENCES reference ON UPDATE CASCADE
+);
+
 
 CREATE TABLE gene (
 	tax_id VARCHAR REFERENCES taxonomy ON UPDATE CASCADE,
@@ -71,21 +63,25 @@ CREATE TABLE gene (
 
 CREATE TABLE omics (
 	dataset_id INT PRIMARY KEY,
-	omics_type VARCHAR
+	omics_type VARCHAR,
 	reference INT REFERENCES reference ON UPDATE CASCADE
 );
 
-
-CREATE TABLE ref_signal (
-	reference_id INT REFERENCES reference ON DELETE CASCADE ON UPDATE CASCADE,
-	signal_id INT REFERENCES signal ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY (reference_id, signal_id)
+CREATE TABLE signal (
+	signal_id SERIAL PRIMARY KEY,
+	signal_supercategory VARCHAR,
+	signal_family VARCHAR,
+	signal_trivial_name VARCHAR,
+	signal_systematic_name VARCHAR,
+	signal_chemical_formula VARCHAR,
+	reference_id INT REFERENCES reference ON UPDATE CASCADE,
+	structure_img BYTEA
 );
 
 /* LEVEL 2 TABLES */
 CREATE TABLE analysed_taxons (
 	dataset_id INT REFERENCES omics ON DELETE CASCADE ON UPDATE CASCADE,
-	tax_id CHAR(30) REFERENCES taxonomy ON UPDATE CASCADE,
+	tax_id CHAR(30) REFERENCES taxonomy ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY (dataset_id, tax_id)
 );
 
@@ -94,14 +90,35 @@ CREATE TABLE composite_gene (
 	tax_id VARCHAR,
 	gene_name VARCHAR,
 	domain_order INT,
-	FOREIGN KEY (domain_id) REFERENCES domain(domain_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (tax_id, gene_name) REFERENCES gene (tax_id, gene_name) ON UPDATE CASCADE,
+	FOREIGN KEY (domain_id) REFERENCES domain (domain_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (tax_id, gene_name) REFERENCES gene (tax_id, gene_name) ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY(domain_id, tax_id, gene_name)
 );
 
-CREATE TABLE pathway_genes (
-	pathway_id INT REFERENCES pathway ON DELETE CASCADE ON UPDATE CASCADE,
-	tax_id VARCHAR REFERENCES taxonomy ON DELETE CASCADE ON UPDATE CASCADE,
-	pathway_name VARCHAR,
-	PRIMARY KEY(pathway_id, tax_id)
+CREATE TABLE environment_members (
+	env_id INT REFERENCES environment ON DELETE CASCADE ON UPDATE CASCADE,
+	tax_id CHAR(30) REFERENCES taxonomy ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (env_id, tax_id)
 );
+
+CREATE TABLE function (
+	tax_id VARCHAR,
+	gene_name VARCHAR,
+	bio_process_id INT,
+	function VARCHAR,
+	reference_id INT REFERENCES reference ON UPDATE CASCADE,
+	FOREIGN KEY (tax_id, gene_name) REFERENCES gene (tax_id, gene_name) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (bio_process_id) REFERENCES bio_process (bio_process_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY (tax_id, gene_name, bio_process_id, function)
+);
+
+CREATE TABLE pathway_genes (
+	pathway_id INT,
+	tax_id VARCHAR,
+	gene_name VARCHAR,
+	pathway_name VARCHAR,
+	FOREIGN KEY (pathway_id) REFERENCES pathway (pathway_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (tax_id, gene_name) REFERENCES gene (tax_id, gene_name) ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY(pathway_id, tax_id, gene_name)
+);
+
--- database/admin_scripts/03_drop_tables.sql
+++ database/admin_scripts/03_drop_tables.sql
@@ -1,20 +1,21 @@
 /* LEVEL 2 TABLES */
+DROP TABLE analysed_taxons;
+DROP TABLE composite_gene;
+DROP TABLE environment_members;
+DROP TABLE function;
 DROP TABLE pathway_genes;
-DROP TABLE composite_genes;
+
 
 /* LEVEL 1 TABLES */
-DROP TABLE analysed_taxons;
-DROP TABLE environment_members;
+DROP TABLE bio_process_pathways;
+DROP TABLE environment;
 DROP TABLE gene;
-DROP TABLE ref_signal; 
-DROP TABLE ref_omics;
+DROP TABLE omics;
+DROP TABLE signal;
 
 /* LEVEL O TABLES */
 DROP TABLE bio_process;
 DROP TABLE domain;
-DROP TABLE environment;
-DROP TABLE omics;
 DROP TABLE pathway;
 DROP TABLE reference;
-DROP TABLE signal;
 DROP TABLE taxonomy;
