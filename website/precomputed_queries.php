976842e5c5e9d12c7fbaee1d23ea8708730b2959
Charles <charles@7138-50.snv.jussieu.fr>
2019-02-06 17:02:15 +0100

    rm signal_id to seq

--- website/precomputed_queries.php
+++ website/precomputed_queries.php
@@ -16,7 +16,7 @@
 			include 'includes/queries.php';
 
 			// Connect DB
-			$dbconn = pg_connect('host=localhost dbname=quorum_db user=charles password=openit');
+			$dbconn = pg_connect('host=localhost dbname=quorum_db user=visitor password=toto');
 			if (!$dbconn) {
 				echo('An error occurred.\n');
 				exit;
@@ -30,23 +30,24 @@
 						<button name="do_query_1">List all signal families</button>
 					</form>
 				</li>
-				<li>
+	<!-- 			<li>
 					<form action="#" method="post">
 						Type in Signal id:<input type="text" name="query_signal_id">
 						<button name="do_query_2">Signal id to gene sequences</button>
 					</form>
-				</li>
+				</li> -->
 			</ul>
 		</div>
 
 		<?php
 			if(isset($_POST['do_query_1'])){
  				$result = list_all_signal_families($dbconn);
-			} elseif(isset($_POST['do_query_2'])) {
-				if(isset($_POST['query_signal_id'])) {
-					$result = signal_id_to_gene_sequences($dbconn, $_POST['query_signal_id']);
-				}
-			}
+ 			}
+			// } elseif(isset($_POST['do_query_2'])) {
+			// 	if(isset($_POST['query_signal_id'])) {
+			// 		$result = signal_id_to_gene_sequences($dbconn, $_POST['query_signal_id']);
+			// 	}
+			// }
 			print_table($result);
 			pg_free_result($result);
 		?>
