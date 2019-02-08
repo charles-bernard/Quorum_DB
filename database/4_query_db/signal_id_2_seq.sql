7883d11c3f65e84dbfb8f519e696b86e67420185
charles <charles.bernard@cri-paris.org>
2019-02-08 17:06:22 +0100

    update

--- database/4_query_db/signal_id_2_seq.sql
+++ database/4_query_db/signal_id_2_seq.sql
@@ -1,6 +1,6 @@
 \prompt	'Type in the signal_id you want to display the sequences of \n-> ' QUERY_SIGNAL_ID
 
-SELECT 
+SELECT
 	function.species_name, 
 	function.gene_name,
 	function.function,
@@ -10,10 +10,11 @@ FROM
 	function
 	LEFT JOIN sequence 
 	ON
-		function.gene_name = sequence.gene_name
-		AND function.species_name = sequence.species_name 
-WHERE function.signal_id = :QUERY_SIGNAL_ID AND bio_process_id = 1
+		(function.species_name = sequence.species_name
+		AND function.gene_name = sequence.gene_name)
+WHERE function.signal_id = :QUERY_SIGNAL_ID AND bio_process_id = 1 AND reference_id != 0
 ORDER BY 
+	-- function.reference_id,
 	array_position(
 		ARRAY['synthase', 'modifier_transporter', 'receptor', 'response_regulator']::VARCHAR[], 
 		function.function),
