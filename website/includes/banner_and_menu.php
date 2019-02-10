735538342794749ddc70a57e01ffbf2ab3d6d468
charles <charles.bernard@cri-paris.org>
2019-02-10 02:01:52 +0100

    update

--- website/includes/banner_and_menu.php
+++ website/includes/banner_and_menu.php
@@ -6,17 +6,18 @@
 	  The smallest number of members who must be present at a meeting in order for decisions to be officially made {Cambridge Academic Content Dictionary}
 			</font></pre>
 		</td>
-		<td width="10%"><font color="white">QuorumDB</font></td>
+		<td width="10%"><font color="white"><b>QuorumDB</b></font></td>
 	</tr>
 </table>
 
 <nav id="menu">        
 	<div class="element_menu">
-		<table border=2 width="100%">
+		<table width="100%" style="background-color: #ff3333" border=2>
         	<tr>
-	            <td width="8%"><a href="index.php">Home</a></td>
-				<td width="92%"><a href="precomputed_queries.php">Pre-computed queries</a></td>
+	            <td width="8%"><a href="index.php"><b><font color="white">Home</font></b></a></td>
+				<td width="92%"><a href="precomputed_queries.php"><b><font color="white">Pre-computed queries</font></b></a></td>
         	</tr>
         </table>
     </div>    
 </nav>
+<br>
--- website/includes/display_query.php
+++ website/includes/display_query.php
@@ -6,73 +6,23 @@
 		// this return the number of fields in the queried table
 		$n_fields = pg_num_fields($query_result);
 
-		// TABLE HEADER
-		// Print each column of the table in bold
-		echo('<tr>');
-		for($i=0;$i < $n_fields;$i++) {
-			$column_name = pg_field_name($query_result, $i);
-			echo('<td>' . '<b>' . $column_name . '&nbsp;&nbsp;' . '</b>' . '</td>');
-		}
-		echo('</tr>');
-
-		// FILTER ROW
-		// For each field, this will consist in:
-		//   1) an entry typing button (sending 'filter_val') 
-		//   2) a submit button (sending 'do_filter')
-		//   3) a submit button (sending 'do_sort_asc')
-		//   4) a submit button (sending 'do_sort_desc')
-		// Column names whose values are numbers are not considered
-		// because the filter query is based on string regexp
-		if(basename($_SERVER['PHP_SELF']) == "index.php" || basename($_SERVER['PHP_SELF']) == "index.php#") {
-			$bgcol = "Teal";
-			// first column filtering and sorting is only enabled for table species
-			if($curr_table == "species") {
-				$i = 0;
-			} else {
-				echo('<tr>');
-				echo('<td bgcolor="' . $bgcol . '"><font color="White">filter by</font></td>');
-				$i = 1;
-			}
-			for($i=$i;$i < $n_fields;$i++) {
-				$column_name = pg_field_name($query_result, $i);
-				if($column_name != "pubmed_id" and $column_name != "ncbi_tax_id") {
-					echo('<td bgcolor="' . $bgcol . '">');
-					echo('<form action="" method="post">');
-					echo('<input type="hidden" name="filtered_col" value="' . $column_name .'">');
-					echo('<input type="hidden" name="chosen_table" value="' . $curr_table .'">');
-					echo('<table><tr>');
-					echo('<td><input type="text" name="filter_val" style="width:90px"></td>');
-					echo('<td><button name="do_sort_desc">&#8593;</button></td>');
-					echo('</tr><tr>');
-					echo('<td><button name="do_filter" style="width:105px">Filter</button></td>');
-					echo('<td><button name="do_sort_asc">&#8595;</button></td>');
-					echo('</tr></table>');
-					echo('</form>');
-					echo('</td>');
-				} else {
-					echo('<td bgcolor="' . $bgcol . '"></td>');
-				}
-			}
-			echo('</tr>');
-		}
-
-
 		// FIELD DISPLAY SPECIFICITIES
 		// This will link some field names to relevant internal or external resources such as NCBI taxonomy
-		// $font_array = array();
+		$font_array = array();
 		$signal_link = array();
 		$ref_link = array();
 		$species_link = array();
 		$fapath_link = array();
+		$align = array();
 
 		for($i=0;$i < $n_fields;$i++) {
 			$column_name = pg_field_name($query_result, $i);
 
-			// if($column_name == "fa_seq") {
-			// 	array_push($font_array, "Courier");
-			// } else {
-			// 	array_push($font_array, "\"\"");
-			// }
+			if($column_name == "fa_seq") {
+				array_push($font_array, "Courier New");
+			} else {
+				array_push($font_array, "\"\"");
+			}
 
 			if($column_name == "signal_id" || $column_name == "id") {
 				array_push($signal_link, true);
@@ -97,7 +47,67 @@
 			} else {
 				array_push($fapath_link, false);
 			}
+
+			if($column_name == "genes" or $column_name == "full_lineage" 
+				or $column_name == "full_lineage_ranks" or $column_name == "functions") {
+				array_push($align, "left");
+			} else {
+				array_push($align, "center");
+			}
 		}
+
+		// TABLE HEADER
+		// Print each column of the table in bold
+		echo('<tr>');
+		for($i=0;$i < $n_fields;$i++) {
+			$column_name = pg_field_name($query_result, $i);
+			echo('<td align="' . $align[$i] . '">' . '<b>' . $column_name . '&nbsp;&nbsp;' . '</b>' . '</td>');
+		}
+		echo('</tr>');
+
+		// FILTER ROW
+		// For each field, this will consist in:
+		//   1) an entry typing button (sending 'filter_val') 
+		//   2) a submit button (sending 'do_filter')
+		//   3) a submit button (sending 'do_sort_asc')
+		//   4) a submit button (sending 'do_sort_desc')
+		// Column names whose values are numbers are not considered
+		// because the filter query is based on string regexp
+		if(basename($_SERVER['PHP_SELF']) == "index.php" || basename($_SERVER['PHP_SELF']) == "index.php#") {
+			$bgcol = "Teal";
+			// first column filtering and sorting is only enabled for table species
+			if($curr_table == "species" or $curr_table == "gene") {
+				$i = 0;
+			} else {
+				echo('<tr>');
+				echo('<td align="center" bgcolor="' . $bgcol . '"><font color="White">filter by</font></td>');
+				$i = 1;
+			}
+			for($i=$i;$i < $n_fields;$i++) {
+				$column_name = pg_field_name($query_result, $i);
+				if($column_name != "pubmed_id" and $column_name != "ncbi_tax_id") {
+					echo('<td align="' . $align[$i] . '" bgcolor="' . $bgcol . '">');
+					echo('<form action="" method="post">');
+					echo('<input type="hidden" name="filtered_col" value="' . $column_name .'">');
+					echo('<input type="hidden" name="chosen_table" value="' . $curr_table .'">');
+					echo('<table><tr>');
+					echo('<td><input type="text" name="filter_val" style="width:90px"></td>');
+					echo('<td><button name="do_sort_desc">&#8593;</button></td>');
+					echo('</tr><tr>');
+					echo('<td><button name="do_filter" style="width:105px">Filter</button></td>');
+					echo('<td><button name="do_sort_asc">&#8595;</button></td>');
+					echo('</tr></table>');
+					echo('</form>');
+					echo('</td>');
+				} else {
+					echo('<td bgcolor="' . $bgcol . '"></td>');
+				}
+			}
+			echo('</tr>');
+		}
+
+
+
 
 		// TABLE CONTENT
 		$k = 1;
@@ -108,7 +118,7 @@
 				echo('<tr bgcolor="#E8E8E8">');
 			}
 			for($i=0;$i<$n_fields;$i++) {
-				echo('<td>');
+				echo('<td align="' . $align[$i] . '">');
 				if($signal_link[$i]) {
 					echo('<a href="signal_report.php?val=' . $record[$i] . '" target="_blank">');
 					echo('browse id ' . $record[$i] . '</a>');
